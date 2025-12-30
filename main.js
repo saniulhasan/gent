6c1c94db5ed4202fd326b8de35fbfa1980bc0123
saniowl <saniowlhasan2@gmail.com>
2025-12-30 15:53:23 +0600

    second part

--- main.js
+++ main.js
@@ -21,3 +21,42 @@ UnicornStudio.addScene({
     .catch((err) => {
       console.error(err);
     });
+
+
+
+    
+var videoContainer = document.querySelector(".section-2");
+var blob = document.querySelector("#blob");
+
+videoContainer.addEventListener("mousemove", function(details){
+  blob.style.transform = `translate(${details.clientX * 0.5}px, ${details.clientY * 0.3}px)`
+})
+
+
+// GSAP Code for section 2
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+ 
+  var part1 = gsap.timeline({
+    scrollTrigger:{
+      trigger: "#section2-1",
+      start:"10% 100%",
+      end:"90% 0%",
+      scrub: true,
+      // markers: true,
+      duration: 1,
+    }
+  });
+  part1.to("#top-nav-sec2",{
+    backgroundColor: "#fd7024"
+  }, 'a')
+  .from("#section2-1 h1, #section2-1 p, #section2-1 img",{
+    opacity: 0,
+    y: -850,
+  }, 'a')
+  .to("#section2-1 h1, #section2-1 p, #section2-1 img",{
+    opacity:0,
+    y:850,
+  })
+})
