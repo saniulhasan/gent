0411eb8a3ac17771970d562d938f6382ab3466f1
saniowl <saniowlhasan2@gmail.com>
2026-01-01 03:23:22 +0600

    second part done

--- main.js
+++ main.js
@@ -60,8 +60,71 @@ document.addEventListener("DOMContentLoaded", (event) => {
     y:850,
   })
 
-
+    var part2 = gsap.timeline({
+    scrollTrigger:{
+      trigger: "#section2-2",
+      start:"10% 100%",
+      end:"90% 0%",
+      scrub: true,
+      // markers: true,
+      duration: 1,
+    }
+  });
+  part2.to("#top-nav-sec2-1",{
+    backgroundColor: "#fd7024"
+  }, 'b')
+  .from("#section2-2 h1, #section2-2 p, #section2-2 img",{
+    opacity: 0,
+    y: -850,
+  }, 'b')
+  .to("#section2-2 h1, #section2-2 p, #section2-2 img",{
+    opacity:0,
+    y:850,
+  })
     
+  var part3 = gsap.timeline({
+    scrollTrigger:{
+      trigger: "#section2-3",
+      start:"10% 100%",
+      end:"90% 0%",
+      scrub: true,
+      // markers: true,
+      duration: 1,
+    }
+  });
+  part3.to("#top-nav-sec2-2",{
+    backgroundColor: "#fd7024"
+  }, 'c')
+  .from("#section2-3 h1, #section2-3 p, #section2-3 img",{
+    opacity: 0,
+    y: -850,
+  }, 'c')
+  .to("#section2-3 h1, #section2-3 p, #section2-3 img",{
+    opacity:0,
+    y:850,
+  })
+
+  var part4 = gsap.timeline({
+    scrollTrigger:{
+      trigger: "#section2-4",
+      start:"10% 100%",
+      end:"90% 0%",
+      scrub: true,
+      // markers: true,
+      duration: 1,
+    }
+  });
+  part4.to("#top-nav-sec2-3",{
+    backgroundColor: "#fd7024"
+  }, 'c')
+  .from("#section2-4 h1, #section2-4 p, #section2-4 img",{
+    opacity: 0,
+    y: -850,
+  }, 'c')
+  .to("#section2-4 h1, #section2-4 p, #section2-4 img",{
+    opacity:0,
+    y:850,
+  })
 
 })
 
